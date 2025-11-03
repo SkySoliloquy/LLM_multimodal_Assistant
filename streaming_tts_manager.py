83fa6cd995e584dd7e4b953ffb7bc6e9e8040fcc
onlineace <3111576469@qq.com>
2025-11-03 17:47:02 +0800

    更新了一版音色，现在她更生动了。

--- streaming_tts_manager.py
+++ streaming_tts_manager.py
@@ -267,19 +267,21 @@ class StreamingTTSManager:
             success = self.tts_client.text_to_speech(
                 text=chunk,
                 #ref_audio_path=r"D:\Project\SenseVoice-main\U_Offical.mp3",  # 使用固定的参考音频路径
-                ref_audio_path="D:\Project\SenseVoice-main\Athna.wav",
+                ref_audio_path="D:\Project\SenseVoice-main\谁能想到佳怡最后会到火星上去呢？.wav",
                 output_path=output_path,
                 text_lang="zh",
                 prompt_lang="zh",
                 #prompt_text="新年特别直播，用味觉巡游这片大地。",
-                prompt_text="伊利奥斯的古代废墟是受国际保护的历史遗址。",
+                #prompt_text="伊利奥斯的古代废墟是受国际保护的历史遗址。",
+                prompt_text="谁能想到佳怡最后会到火星上去呢。",
                 top_k=10,
-                top_p=0.8,
+                top_p=1,
                 temperature=1,
                 speed_factor=1,
                 text_split_method="cut4",
-                batch_size=4,
-                sample_steps=4
+                batch_size=2,
+
+                #sample_steps=4 #采样率，适用v4
             )
             
             processing_time = time.time() - start_time
